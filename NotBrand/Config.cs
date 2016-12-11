77e3d6ffce823f07603bb407f82a0576c923c398
FireBuddy <aijun@live.ca>
2016-12-11 12:57:57 -0500

    Update Config.cs

--- NotBrand/Config.cs
+++ NotBrand/Config.cs
@@ -1,5 +1,11 @@
 ﻿using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
+using System;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
 
 namespace NotBrand
 {
